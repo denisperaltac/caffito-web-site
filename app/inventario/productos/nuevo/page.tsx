656b3ec03e6eaae0ec8618e7de920c095b6b54e4
Denis Peralta <denisperaltac@gmail.com>
2024-01-17 12:23:01 -0300

    Corrección error naming

--- app/inventario/productos/nuevo/page.tsx
+++ app/inventario/productos/nuevo/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { NewProductComponent } from "./component";
 
-export default function newProductPage() {
+export default function NewProductPage() {
   const [form, setForm] = useState({});
 
   return <NewProductComponent form={form} setForm={setForm} />;
--- components/btn/BtnIcon.tsx
+++ components/btn/BtnIcon.tsx
@@ -33,7 +33,7 @@ const BtnIcon: React.FC<BtnIconProps> = ({
         <button
           type="button"
           onClick={action}
-          className={`bg-gray-600 hover:filter hover:shadow-lg hover:scale-105 text-slate-100 rounded max-w-[200px] max-h-8 flex items-center justify-between py-2 px-2 border transition ${
+          className={`bg-gray-600 hover:filter hover:shadow-lg hover:scale-105 text-slate-100 rounded max-w-[200px] max-h-10 flex items-center justify-between py-2 px-2 border transition ${
             disabled && "pointer-events-none opacity-30"
           }`}
         >
